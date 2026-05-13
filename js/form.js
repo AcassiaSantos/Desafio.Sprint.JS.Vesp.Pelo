4798c0e47655f72f62609f17da9ad9a78b474d00
AcassiaSantos <acassiamsantos85@gmail.com>
2026-05-13 15:33:55 -0300

    Alterando a página do contato

--- js/form.js
+++ js/form.js
@@ -1,27 +1,38 @@
+class Contato {
 
-//class contato
+    constructor(
+        nome,
+        email,
+        telefone,
+        contato,
+        mensagem
+    ) {
 
-class contato {
-    
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.contato = contato;
+        this.mensagem = mensagem;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+    let data = new Contato(
 
-function Enviar() {
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value,
+        form.elements.namedItem("mensagem").value
 
-    var nome = document.getElementById("nomeid");
+    );
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+    console.log(data);
 
+    alert(
+        "Obrigado " +
+        data.nome +
+        ", seus dados foram enviados!"
+    );
 }
